6c57dd86abb6d24e594c40e788745018b5545ece
unknown <ovanalviansyah217@gmail.com>
2021-10-12 15:29:26 +0700

    Push tugas

---
+++ javabasic1.java
@@ -0,0 +1,22 @@
+public class javabasic1{
+    //function is a method
+    public static void main (String[] args){
+        // first name , lastname, age, phone number, address, and other
+
+        String  firstname   = "Ovan Fifat ";
+        String  lastname    = "Alviansyah";
+        int     age         = 19;
+        String  phone       = "082259961216";
+        String  address     = "Penawar Rejo";
+        char    bloodtype   = 'B';
+        String  hobby       = "something related to coding and design";
+
+        System.out.println("Name         = "+firstname + lastname);
+        System.out.println("Age          = "+age+"th");
+        System.out.println("Number Phone = "+phone);
+        System.out.println("Address      = "+address);
+        System.out.println("Blood Type   = "+bloodtype);
+        System.out.println("Hobby        = "+hobby);
+    }
+    
+}
